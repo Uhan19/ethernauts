9119ac85f14744bf3c1f91c0ca130751156ae7f3
Yuehan Duan <yuehanduan@Yuehans-MacBook-Pro.local>
2023-01-20 23:25:35 +0100

    Hack coinflip contract

--- contracts/1. CoinFlip/CoinFlipHelper.ts
+++ contracts/1. CoinFlip/CoinFlipHelper.ts
@@ -3,6 +3,11 @@ import { ethers } from "hardhat";
 
 const helper = async (victim: any, attacker: any) => {
   // add code here that will help you pass the test
-};
+  let wins = 0;
+  while (wins < 10) {
+    await attacker.hackContract()
+    wins++;
+  }
+ };
 
 export default helper;
